9e920453ef93a8dc8993d88dccd6b064ce8a8b10
jialin-wu-02 <jialin.wu.02@gmail.com>
2021-10-27 17:20:52 -0700

    update backend homework

--- client/favoritescript.js
+++ client/favoritescript.js
@@ -1,29 +1,48 @@
 (() => {
-    // makeAPOD is used to create a APOD node in the following format:
-    // <div class="apod">
-    //     <small id="apod-date"> 02-21-2021 </small>
-    //     <img id="apod-image" width="200px" src="https://apod.nasa.gov/apod/image/2102/rosette_goldman_960.jpg" alt="">
-    // </div>
-    const makeAPOD = (url, date) => {
-        var div = document.createElement("div");
-        div.className = "apod";
-        var small = document.createElement("small");
-        small.id = "apod-date";
-        small.innerText = date;
-        var img = document.createElement("img");
-        img.src = url;
-        img.style.width = "200px"
-        div.appendChild(small);
-        div.appendChild(img);
-        return div
-    }
+  // makeAPOD is used to create a APOD node in the following format:
+  // <div class="apod">
+  //     <small id="apod-date"> 02-21-2021 </small>
+  //     <img id="apod-image" width="200px" src="https://apod.nasa.gov/apod/image/2102/rosette_goldman_960.jpg" alt="">
+  // </div>
+  const makeAPOD = (url, date) => {
+    var div = document.createElement("div");
+    div.className = "apod";
+    var small = document.createElement("small");
+    small.id = "apod-date";
+    small.innerText = date;
+    var img = document.createElement("img");
+    img.src = url;
+    img.style.width = "200px";
+    div.appendChild(small);
+    div.appendChild(img);
+    return div;
+  };
 
-    // TODO: Fetch a list of APODs from the database.
-    // Here the apods are filled with dummy data.
-    apods = [["https://apod.nasa.gov/apod/image/2102/rosette_goldman_960.jpg", "02-21-2021"], ["https://apod.nasa.gov/apod/image/2102/rosette_goldman_960.jpg", "02-20-2021"]]
-    var al = document.getElementById("apod-list");
-    for (apod of apods) {
-        console.log(apod)
-        al.appendChild(makeAPOD(apod[0], apod[1]))
-    }
-})()
+  // TODO: Fetch a list of APODs from the database.
+  // You code should be something like:
+  // fetch(...)
+  //    .then(response => response.json())
+  //    .then(json => {
+  //        apods = json...
+  //        for (apod of apods) {
+  //           console.log(apod);
+  //           al.appendChild(makeAPOD(apod[0], apod[1]));
+  //        }
+  //    });
+  // Here the apods are filled with dummy data.
+  apods = [
+    [
+      "https://apod.nasa.gov/apod/image/2102/rosette_goldman_960.jpg",
+      "02-21-2021",
+    ],
+    [
+      "https://apod.nasa.gov/apod/image/2102/rosette_goldman_960.jpg",
+      "02-20-2021",
+    ],
+  ];
+  var al = document.getElementById("apod-list");
+  for (apod of apods) {
+    console.log(apod);
+    al.appendChild(makeAPOD(apod[0], apod[1]));
+  }
+})();
--- client/homescript.js
+++ client/homescript.js
@@ -1,23 +1,51 @@
-console.log("script connected.")
+console.log("script connected.");
 
-var heart_status = 0 // 0 is empty and 1 is filled.
+var heart_status = 0; // 0 is empty and 1 is filled
+var date = new Date();
+var year = date.getFullYear();
+var month = date.getMonth();
+var day = date.getDate();
+
+const previousDate = (year, month, day) => {
+  if (day - 1 < 1) {
+    month -= 1;
+  } else {
+    day -= 1;
+  }
+  return [year, month, day];
+};
+
+const dateToString = (year, month, day) =>
+  String(year) + "-" + String(month) + "-" + String(day);
 
 document.getElementById("heart-button").addEventListener("click", () => {
-    let heart = document.getElementById("heart-button");
-    if (heart_status == 0) {
-        heart.src = "static/heart-filled.png"
-        heart_status = 1
-        // TODO: update the database and mark this image as a favorite image.
-    } else {
-        heart_status = 0
-        heart.src = "static/heart.png"
-        // TODO: update the database and un-mark this image as a favorite image.
-    }
-})
+  let heart = document.getElementById("heart-button");
+  if (heart_status == 0) {
+    heart.src = "static/heart-filled.png";
+    heart_status = 1;
+    // TODO: update the database and mark this image as a favorite image.
+  } else {
+    heart_status = 0;
+    heart.src = "static/heart.png";
+    // TODO: update the database and un-mark this image as a favorite image.
+  }
+});
 
 document.getElementById("next-button").addEventListener("click", () => {
-    document.getElementById("heart-button").src = "static/heart.png";
-    heart_status = 0
-    // TODO: Get the image url, title, description, and date from the database using Fetch.
-    // you can use let date = document.getElementById("apod-date"); to change the date.
-})
+  document.getElementById("heart-button").src = "static/heart.png";
+  heart_status = 0;
+  [year, month, day] = previousDate(year, month, day);
+  fetch(
+    "https://api.nasa.gov/planetary/apod?api_key=xRSoeMQHQJ5IiKwhHnOYfppEE05ADhjDFg18Qdwx&date=" +
+      dateToString(year, month, day)
+  )
+    .then((r) => r.json())
+    .then((r) => {
+      console.log("current APOD data:");
+      console.log(r);
+      document.getElementById("apod-date").innerHTML = r.date;
+      document.getElementById("apod-image").src = r.url;
+      document.getElementById("apod-title").innerHTML = r.title;
+      document.getElementById("apod-p").innerHTML = r.explanation;
+    });
+});
